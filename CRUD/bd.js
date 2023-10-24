cc1bbda75f28e3403a9541edb1e345941777746d
Aluno <Aluno@fatecdiadema.com>
2023-10-24 12:28:17 -0300

    criacao do novo

--- CRUD/bd.js
+++ CRUD/bd.js
@@ -1,165 +1,121 @@
-function local() {
-    let dados = [{
+function banco() {
+    const dados = [{
         "id": null,
         "nome": "cleito",
+        "login": "cleito",
         "senha": "senha",
         "email": "teste@gmail.com",
-        "telefone": 910203454,
-        "cpf": 12345695123,
-        "data_nascimento": "10/02/2022"
+        "cpf": 12345695123
     },
 
     {
         "id": null,
         "nome": "cleitin",
+        "login": "cleitin",
         "senha": "senhaa",
         "email": "teste2@gmail.com",
-        "telefone": 910203454,
-        "cpf": 12345695123,
-        "data_nascimento": "10/02/2022"
+        "cpf": 12345695123
     },
 
     {
         "id": null,
         "nome": "cleitao",
+        "login": "cleitao",
         "senha": "senha0",
         "email": "teste3@gmail.com",
-        "telefone": 910203454,
-        "cpf": 12345695123,
-        "data_nascimento": "10/02/2022"
-    }
-    ]
-    let n = JSON.stringify(dados);
-    localStorage.setItem("tds", n);
-    adicionar()
-    return dados
+        "cpf": 12345695123
+    }]
+    let ds = JSON.stringify(dados)
+
+    localStorage.setItem("bd", ds)
 }
 
-function logon() {
-    const dados = JSON.parse(localStorage.getItem("tds"))
-    let login = document.querySelector("#inputlogin").value
-    let senha = document.querySelector("#inputsenha").value
+function criar() {
+    const dados = JSON.parse(localStorage.getItem("bd"))
+    let id = document.querySelector("#id").value
+    let lg = document.querySelector("#Login").value
+    let sn = document.querySelector("#senha").value
+    let nm = document.querySelector("#Nome").value
+    let cpf = document.querySelector("#cpf").value
+    let email = document.querySelector("#email").value
 
-    for (let i = 0; dados.length > i; i++) {
-        if (dados[i] == null) {
-            alert("Verificando")
-            //  alert("encontrou: " + dados[i].nome + ":" + i)
-        } else {
-            if (login == dados[i].nome && senha == dados[i].senha) {
-                console.log("conectado")
-                let n = JSON.stringify(dados[i]);
-                sessionStorage.setItem("user", n)
-                login()
-                break
-            }
+    let data = { id: Date.now(),id:id, login: lg, senha: sn,nome:nm,email:email,cpf:cpf }
+
+    dados.push(data)
+
+    const ds = JSON.stringify(dados)
+
+    localStorage.setItem("bd", ds)
+
+    limpar()
+}
+
+function ler() {
+    const dados = JSON.parse(localStorage.getItem("bd"))
+
+    let nm = document.querySelector("#Login").value
+
+    for (let i = 0; i < dados.length; i++) {
+        if (dados[i].Login == nm) {
+            console.log(dados[i].id + ":" + dados[i].Login + ":" + dados[i].senha)
+
+            document.querySelector("#id").value = dados[i].id
+            document.querySelector("#Login").value = dados[i].Login
+            document.querySelector("#senha").value = dados[i].senha
+            document.querySelector("#Nome").value = dados[i].nome
+            document.querySelector("#cpf").value = dados[i].cpf
+            document.querySelector("#email").value = dados[i].email
         }
-    }
-}
-//SessionStore getItem
-function logado() {
-    let dados = JSON.parse(sessionStorage.getItem("user"))
-    let nome = dados.nome
-    document.getElementById("nome").innerHTML = "Bem vindo " + nome
-    return nome
-}
-//SessionStore remove Item
-function logaout() {
-    sessionStorage.removeItem("user")
-    let url = "localStorage.html"
-    window.open(url)
-    window.close()
-}
-
-
-
-function adicionar() {
-    var ClienteArray = JSON.parse(localStorage.getItem("tds"))
-    let email = document.querySelector("#inputemail").value
-    let senha = document.querySelector("#inputsenha").value
-    let nome = document.querySelector("#inputnome").value
-    let data_nascimento = document.querySelector("#inputdata_nascimento").value
-    let cpf = document.querySelector("#inputcpf").value
-    let telefone = document.querySelector("#inputtelefone").value
-    let user = { id: Date.now(), email: email, senha: senha, nome: nome, data_nascimento: data_nascimento, cpf: cpf, telefone: telefone }
-    ClienteArray.push(user)
-    localStorage.setItem("tds", JSON.stringify(ClienteArray))
-    redi()
-}
-function redi() {
-    window.location.href = "./crud.html";
-    //alert("Registro adicionado.")
-    //window.location.replace("./crud.html");
-}
-function login() {
-    window.location.href = "./login.html";
-    //alert("Registro adicionado.")
-    //window.location.replace("./crud.html");
-}
-function buscar() {
-    var dados = JSON.parse(localStorage.getItem("tds"))
-    let login = document.querySelector("#inputlogin").value
-
-    for (let i = 0; dados.length > i; i++) {
-        if (dados[i] == null && dados[i] != login) {
-            alert("Verificando")
-        } else {
-            if (login == dados[i].nome) {
-                document.querySelector("#inputlogin").value = dados[i].nome
-                document.querySelector("#inputsenha").value = dados[i].senha
-                break
-            }
-        }
-    }
-}
-
-function tabela() {
-    var dados = JSON.parse(localStorage.getItem("tds"))
-    for (let i = 0; dados.length > i; i++) {
-        document.querySelector("#inputusuario").innerHTML = dados[i].nome
-        document.querySelector("#inputpass").innerHTML = dados[i].senha
     }
 }
 
 function atualizar() {
-    var dados = JSON.parse(localStorage.getItem("tds"))
-    localStorage.removeItem("tds")
-    let login = document.querySelector("#inputlogin").value
-    let senha = document.querySelector("#inputsenha").value
+    var dados = JSON.parse(localStorage.getItem("bd"))
+    let login = document.querySelector("#Login").value
+    let senha = document.querySelector("#senha").value
+    let nome = document.querySelector("#Nome").value
+    let cpf = document.querySelector("#cpf").value
+    let email = document.querySelector("#email").value
 
-    for (let i = 0; dados.length > i; i++) {
-        if (id == dados[i].id) {
-            let user = { id: id, nome: login, senha: senha }
-            dados[i] = user
-            localStorage.setItem("tds", JSON.stringify(dados))
-            alert("Atualizado!")
-            break
+    
+
+    for (let i = 0; i < dados.length; i++) {
+        if (login == dados[i].Login) {
+            dados[i].Login = login
+            dados[i].senha = senha
+            dados[i].nome = nome
+            dados[i].cpf = cpf
+            dados[i].email = email
+            localStorage.setItem("bd", JSON.stringify(dados));
+            alert("Atualizado!");
+            limpar()
+            break;
         }
     }
 }
 
-function apagarItemVetor() {
-    let login = document.querySelector("#inputlogin").value
-    var dados = JSON.parse(localStorage.getItem("tds"))
-    localStorage.removeItem("tds")
+function apagar() {
+    var dados = JSON.parse(localStorage.getItem("bd"));
+    let login = document.querySelector("#Login").value;
 
-    for (let i = 0; dados.length > i; i++) {
-        if (dados[i] == null) {
-            alert("Verificando")
-        } else {
-            if (id == dados[i].id && login == dados[i].nome) {
-                delete dados[i]
-                break;
-            }
+    for (let i = 0; i < dados.length; i++) {
+        if (dados[i] && login === dados[i].Login) {
+            dados.splice(i, 1);
+            alert("Deletado")
+            limpar()
+            break;
+
         }
     }
-    localStorage.setItem("tds", JSON.stringify(dados))
+
+    localStorage.setItem("bd", JSON.stringify(dados));
 }
 
-function apagaTudo() {
-    localStorage.removeItem("tds")
-}
-
-function limpar() {
-    document.querySelector("#inputlogin").value = ""
-    document.querySelector("#inputsenha").value = ""
-}
+function limpar(){
+    document.querySelector("#id").value = ""
+    document.querySelector("#Login").value = ""
+    document.querySelector("#senha").value = "" 
+    document.querySelector("#Nome").value = "" 
+    document.querySelector("#cpf").value = "" 
+    document.querySelector("#email").value = "" 
+  }
